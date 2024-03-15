9113f9fb904174ec88bbc42ec4143cda419e7180
Anomali99 <fatiqnur@gmail.com>
2024-03-15 22:48:44 +0700

    update6

--- api/users.py
+++ api/users.py
@@ -1,5 +1,8 @@
 from . import api
 from model.users import Users
+from model.reting import Reting
+from model.transaksi import Transaksi
+from model.detail_transaksi import Detail_Transaksi
 from config import db
 from flask import jsonify, request
 from werkzeug.security import check_password_hash
@@ -13,6 +16,25 @@ def user():
         return jsonify(data)
     else:
         return  jsonify({'message' : 'tidak ada user'})
+    
+@api.route('/user/<id>/remove', methods=['GET'])
+def removeUser(id):
+    users = db.session.query(Users).filter(Users.id == id).first()
+    reting = db.session.query(Reting).filter(Reting.id_user == id).all()
+    transaksi = db.session.query(Transaksi).filter(Transaksi.id_user == id).all()
+    if users:
+        for ret in reting:
+            db.session.delete(ret)
+        for tran in transaksi:
+            dt = db.session.query(Detail_Transaksi).filter(Detail_Transaksi.id_transaksi == tran.id).all()
+            for det in dt:
+                db.session.delete(det)
+            db.session.delete(tran)
+        db.session.delete(users)
+        db.session.commit()
+        return jsonify({'message' : 'user berhasil dihapus'})
+    else:
+        return  jsonify({'message' : 'gagal menghapus'})
 
 @api.route('/user/login', methods=['POST'])
 def login():
@@ -48,7 +70,7 @@ def addPegawai():
     username= request.json['username']
     password= request.json['password']
     telepon = request.json['telepon']
-    level = request.json['level']
+    level = "pegawai"
     try:
         user = Users(nama=nama, username=username, password=password, telepon=telepon, level=level)
         check = db.session.query(Users).filter_by(username=username).first()
--- templates/js/users.js
+++ templates/js/users.js
@@ -1,3 +1,5 @@
+var users = null;
+
 fetch("http://192.168.68.219:5127/user", {
   method: "GET",
   headers: {
@@ -13,6 +15,7 @@ fetch("http://192.168.68.219:5127/user", {
   .then((data) => {
     console.log(data);
     if (!data.message) {
+      users = data;
       data.forEach((item) => {
         var productCard = document.createElement("div");
         productCard.className = "product-card";
@@ -21,15 +24,157 @@ fetch("http://192.168.68.219:5127/user", {
         <div class="row">
         <h1>${item.id}</h1>
         <h1>${item.nama}</h1>
-        <h1>${item.username}</h1>
         <h1>${item.telepon}</h1>
         <h1>${item.level}</h1>
+        <div class="product-icons">
+          <a href="#products" onclick="hapus('${item.id}')"><i data-feather="trash-2"></i></a>
+          <a href="#products" class="item-detail-button" onclick="detailuser('${item.id}')" ><i data-feather="eye"></i
+          ></a>
+        </div>
         </div>
       `;
         document.getElementById("reting").appendChild(productCard);
       });
+      feather.replace();
     }
   })
   .catch((error) => {
     console.error("There was a problem with the fetch operation:", error);
   });
+
+function hapus(id) {
+  fetch("http://192.168.68.219:5127/user/" + id + "/remove", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      if (data.message != "gagal menghapus") {
+        window.location.href = "users.html";
+      }
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
+
+// Modal Box
+const itemDetailModal = document.querySelector("#add-reting-modal");
+const divPassword = document.querySelector("#div-password");
+const divLevel = document.querySelector("#div-level");
+const divId = document.querySelector("#div-id");
+const submitBtn = document.querySelector("#submitBtn");
+const judul = document.querySelector("#judulModal");
+
+const id_userInput = document.querySelector("#id_user");
+const namaInput = document.querySelector("#nama");
+const usernameInput = document.querySelector("#usernameInput");
+const levelInput = document.querySelector("#level");
+const passwordInput = document.querySelector("#password");
+const teleponInput = document.querySelector("#telepon");
+
+function adduser() {
+  itemDetailModal.style.display = "flex";
+  submitBtn.style.display = "inline-block";
+
+  divLevel.style.display = "none";
+  divId.style.display = "none";
+  divPassword.style.display = "flex";
+  judul.innerHTML = "Tambah Pegawai";
+
+  id_userInput.value = "";
+  namaInput.value = "";
+  usernameInput.value = "";
+  levelInput.value = "";
+  passwordInput.value = "";
+  teleponInput.value = "";
+}
+
+function detailuser(id) {
+  itemDetailModal.style.display = "flex";
+
+  divLevel.style.display = "flex";
+  divId.style.display = "flex";
+  divPassword.style.display = "none";
+  submitBtn.style.display = "none";
+  judul.innerHTML = "Detail User";
+
+  id_userInput.setAttribute("readonly", true);
+  namaInput.setAttribute("readonly", true);
+  usernameInput.setAttribute("readonly", true);
+  levelInput.setAttribute("readonly", true);
+  teleponInput.setAttribute("readonly", true);
+
+  users.forEach((user) => {
+    if (user.id == id) {
+      console.log(user);
+      id_userInput.value = user.id;
+      namaInput.value = user.nama;
+      usernameInput.value = user.username;
+      levelInput.value = user.level;
+      teleponInput.value = user.telepon;
+    }
+  });
+}
+
+document.querySelector(
+  ".modal-reting .modal-container-reting .close-icon"
+).onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
+
+function retingclose() {
+  itemDetailModal.style.display = "none";
+}
+
+function tambahkan() {
+  var nama = namaInput.value;
+  var username = usernameInput.value;
+  var password = passwordInput.value;
+  var telepon = teleponInput.value;
+
+  var formData = {
+    nama: nama,
+    username: username,
+    password: password,
+    telepon: telepon,
+  };
+
+  fetch("http://192.168.68.219:5127/user/addpegawai", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      if (!data.message) {
+        // window.location.href = "login.html";
+      }
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
