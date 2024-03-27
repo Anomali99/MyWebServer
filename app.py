0e945c53ab262041427d2c9101110cacd0d6f8ce
Anomali99 <fatiqnur@gmail.com>
2024-03-27 14:11:51 +0700

    hapus yang tidak perlu

--- app.py
+++ app.py
@@ -4,16 +4,8 @@ from config import connect
 
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = "static/upload"
 app.register_blueprint(api_blueprint)
 db = connect(app)
-
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-    response.headers.add('Access-Control-Allow-Methods', 'POST')
-    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5127, debug=False)
